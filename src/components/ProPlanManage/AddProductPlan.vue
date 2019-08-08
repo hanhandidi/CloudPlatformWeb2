94780bda0c617d64b0641a59b08dba4eca4fdede
thpeng <320590996@qq.com>
2019-08-08 20:14:01 +0800

    更改目录结构

---
+++ src/components/ProPlanManage/AddProductPlan.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "AddProductPlan"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProScheduleManage/AddProSchedule.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "AddProSchedule"
+    }
+</script>
+
+<style scoped>
+
+</style>
